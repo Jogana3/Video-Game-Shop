5231031a2808d69f70777e1e0681cb5eed0268ec
Nahuel Godoy <64289121+Jogana3@users.noreply.github.com>
2022-07-21 14:10:35 +0100

    All website archives

---
+++ Tienda/js/products.js
@@ -0,0 +1,61 @@
+const products = [{
+    gameName : 'Yakuza 0',
+    gamePrice : '10.99 $',
+    gamePlatform : 'pc',
+    gameImage : './img/Yakuza-0.jpg'
+},{
+    gameName : 'Elder Scrolls V: Skyrim',
+    gamePrice : '19.99 $',
+    gamePlatform : 'pc',
+    gameImage : './img/skyrim.png'
+},{
+    gameName : 'Elden Ring',
+    gamePrice : '59.99 $',
+    gamePlatform : 'pc',
+    gameImage : './img/elden_ring.jpg'
+},{
+    gameName : 'Minecraft: Bedrock Edition',
+    gamePrice : '17.99 $',
+    gamePlatform : 'pc',
+    gameImage : './img/minecraft.svg'
+},{
+    gameName : 'The Last of Us Remastered',
+    gamePrice : '19.99 $',
+    gamePlatform : 'ps',
+    gameImage : './img/last_of_us.png'
+},{
+    gameName : 'The Last of Us 2',
+    gamePrice : '29.99 $',
+    gamePlatform : 'ps',
+    gameImage : './img/last_of_us_2.jpg'
+},{
+    gameName : 'God of War',
+    gamePrice : '59.99 $',
+    gamePlatform : 'ps',
+    gameImage : './img/god_of_war.png'
+},{
+    gameName : 'FIFA 22',
+    gamePrice : '59.99 $',
+    gamePlatform : 'ps',
+    gameImage : './img/fifa_22.jpg'
+},{
+    gameName : 'Forza Horizon 5',
+    gamePrice : '59.99 $',
+    gamePlatform : 'xbox',
+    gameImage : './img/forza_horizon_5.jpg'
+},{
+    gameName : 'Fallout 4',
+    gamePrice : '39.99 $',
+    gamePlatform : 'xbox',
+    gameImage : './img/fallout_4.png'
+},{
+    gameName : 'Grand Theft Auto V',
+    gamePrice : '59.99 $',
+    gamePlatform : 'xbox',
+    gameImage : './img/gta_v.png'
+},{
+    gameName : 'Gears of War 3',
+    gamePrice : '11.99 $',
+    gamePlatform : 'xbox',
+    gameImage : './img/gears_of_war_3.png'
+}]
